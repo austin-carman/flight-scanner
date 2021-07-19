e908acdafae52ffb55bb4ce025090741235f9229
Austin Carman <abcarman12@gmail.com>
2021-07-19 11:12:14 -0600

    setting up router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
